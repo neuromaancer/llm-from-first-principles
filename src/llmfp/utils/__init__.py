1ef6c6b43e3d2835dac363857933ce59a9ce9fef
Alafate <alafate.abulimiti@gmail.com>
2026-09-26 13:40:41 +0200

    refactor(src): add utilities package

---
+++ src/llmfp/utils/__init__.py
@@ -0,0 +1,13 @@
+"""Small inspection and development helpers."""
+
+from .inspection import (
+    count_parameters,
+    parameter_breakdown,
+    print_parameter_breakdown,
+)
+
+__all__ = [
+    "count_parameters",
+    "parameter_breakdown",
+    "print_parameter_breakdown",
+]
